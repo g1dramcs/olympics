1430bf1996a8afbcbdcb87e758a24fe8ef862b39
g1dramcs <mr.nikas.style@gmail.com>
2024-10-23 18:30:03 +0500

    task array upd

--- array_task.py
+++ array_task.py
@@ -3,12 +3,16 @@ from random import randint
 index = 0
 count = int(input())
 lst = []
-max_sum = 0
+max_sums = []
 
 for i in range(count):
     lst.append(randint(0, 100))
 
 for i in lst:
     for l in lst:
-        print(i + l)
-    
+        s = (i + l)
+        if s % 5 == 0:
+            max_sums.append(s)
+
+print(lst)
+print(max(max_sums))
